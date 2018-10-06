3207d077fb493842f66c9866ba5d4ae70ebb66bf
Trent Nelson <trent@trent.me>
2018-10-06 11:18:46 -0700

    Un-inline two routines for appending integers to unicode strings.

--- src/PerfectHash/RtlOutput.c
+++ src/PerfectHash/RtlOutput.c
@@ -8,11 +8,295 @@ Module Name:
 
 Abstract:
 
-    Temporary stop-gap measure for console output.
+    Implementation of various string manipulation routines.
 
 --*/
 
 #include "stdafx.h"
+
+APPEND_INTEGER_TO_UNICODE_STRING AppendIntegerToUnicodeString;
+
+_Use_decl_annotations_
+BOOLEAN
+AppendIntegerToUnicodeString(
+    PUNICODE_STRING String,
+    ULONG Integer,
+    USHORT NumberOfDigits,
+    WCHAR Trailer
+    )
+/*++
+
+Routine Description:
+
+    This is a helper routine that allows construction of unicode strings out
+    of integer values.
+
+Arguments:
+
+    String - Supplies a pointer to a UNICODE_STRING that will be appended to.
+        Sufficient buffer space must exist for the entire string to be written.
+
+    Integer - The integer value to be appended to the string.
+
+    NumberOfDigits - The expected number of digits for the value.  If Integer
+        has less digits than this number, it will be left-padded with zeros.
+
+    Trailer - An optional trailing wide character to append.
+
+Return Value:
+
+    TRUE on success, FALSE on failure.
+
+--*/
+{
+    USHORT ActualNumberOfDigits;
+    USHORT BytesRequired;
+    USHORT BytesRemaining;
+    USHORT NumberOfZerosToPad;
+    const ULONG Base = 10;
+    ULONG Digit;
+    ULONG Value;
+    ULONG Count;
+    ULONG Bytes;
+    WCHAR Char;
+    PWCHAR Dest;
+
+    //
+    // Verify the unicode string has sufficient space.
+    //
+
+    BytesRequired = NumberOfDigits * sizeof(WCHAR);
+
+    if (Trailer) {
+        BytesRequired += (1 * sizeof(Trailer));
+    }
+
+    BytesRemaining = (
+        String->MaximumLength -
+        String->Length
+    );
+
+    if (BytesRemaining < BytesRequired) {
+        return FALSE;
+    }
+
+    //
+    // Make sure the integer value doesn't have more digits than
+    // specified.
+    //
+
+    ActualNumberOfDigits = CountNumberOfDigitsInline(Integer);
+
+    if (ActualNumberOfDigits > NumberOfDigits) {
+        return FALSE;
+    }
+
+    //
+    // Initialize our destination pointer to the last digit.  (We write
+    // back-to-front.)
+    //
+
+    Dest = (PWCHAR)(
+        RtlOffsetToPointer(
+            String->Buffer,
+            String->Length + (
+                (NumberOfDigits - 1) *
+                sizeof(WCHAR)
+            )
+        )
+    );
+    Count = 0;
+    Bytes = 0;
+
+    //
+    // Convert each digit into the corresponding character and copy to the
+    // string buffer, retreating the pointer as we go.
+    //
+
+    Value = Integer;
+
+    do {
+        Count++;
+        Bytes += 2;
+        Digit = Value % Base;
+        Value = Value / Base;
+        Char = IntegerToWCharTable[Digit];
+        *Dest-- = Char;
+    } while (Value != 0);
+
+    //
+    // Pad the string with zeros if necessary.
+    //
+
+    NumberOfZerosToPad = NumberOfDigits - ActualNumberOfDigits;
+
+    if (NumberOfZerosToPad) {
+        do {
+            Count++;
+            Bytes += 2;
+            *Dest-- = L'0';
+        } while (--NumberOfZerosToPad);
+    }
+
+    //
+    // Update the string with the new length.
+    //
+
+    String->Length += (USHORT)Bytes;
+
+    //
+    // Add the trailer if applicable.
+    //
+
+    if (Trailer) {
+        String->Length += sizeof(WCHAR);
+        String->Buffer[(String->Length - 1) >> 1] = Trailer;
+    }
+
+    return TRUE;
+}
+
+APPEND_LONGLONG_INTEGER_TO_UNICODE_STRING AppendLongLongIntegerToUnicodeString;
+
+_Use_decl_annotations_
+BOOLEAN
+AppendLongLongIntegerToUnicodeString(
+    PUNICODE_STRING String,
+    ULONGLONG Integer,
+    USHORT NumberOfDigits,
+    WCHAR Trailer
+    )
+/*++
+
+Routine Description:
+
+    This is a helper routine that allows construction of unicode strings out
+    of integer values.
+
+Arguments:
+
+    String - Supplies a pointer to a UNICODE_STRING that will be appended to.
+        Sufficient buffer space must exist for the entire string to be written.
+
+    Integer - Supplies the long long integer value to be appended to the string.
+
+    NumberOfDigits - The expected number of digits for the value.  If Integer
+        has less digits than this number, it will be left-padded with zeros.
+
+    Trailer - An optional trailing wide character to append.
+
+Return Value:
+
+    TRUE on success, FALSE on failure.
+
+--*/
+{
+    USHORT ActualNumberOfDigits;
+    USHORT BytesRequired;
+    USHORT BytesRemaining;
+    USHORT NumberOfZerosToPad;
+    const ULONGLONG Base = 10;
+    ULONGLONG Digit;
+    ULONGLONG Value;
+    ULONGLONG Count;
+    ULONGLONG Bytes;
+    WCHAR Char;
+    PWCHAR Dest;
+
+    //
+    // Verify the unicode string has sufficient space.
+    //
+
+    BytesRequired = NumberOfDigits * sizeof(WCHAR);
+
+    if (Trailer) {
+        BytesRequired += (1 * sizeof(Trailer));
+    }
+
+    BytesRemaining = (
+        String->MaximumLength -
+        String->Length
+    );
+
+    if (BytesRemaining < BytesRequired) {
+        return FALSE;
+    }
+
+    //
+    // Make sure the integer value doesn't have more digits than
+    // specified.
+    //
+
+    ActualNumberOfDigits = CountNumberOfLongLongDigitsInline(Integer);
+
+    if (ActualNumberOfDigits > NumberOfDigits) {
+        return FALSE;
+    }
+
+    //
+    // Initialize our destination pointer to the last digit.  (We write
+    // back-to-front.)
+    //
+
+    Dest = (PWCHAR)(
+        RtlOffsetToPointer(
+            String->Buffer,
+            String->Length + (
+                (NumberOfDigits - 1) *
+                sizeof(WCHAR)
+            )
+        )
+    );
+    Count = 0;
+    Bytes = 0;
+
+    //
+    // Convert each digit into the corresponding character and copy to the
+    // string buffer, retreating the pointer as we go.
+    //
+
+    Value = Integer;
+
+    do {
+        Count++;
+        Bytes += 2;
+        Digit = Value % Base;
+        Value = Value / Base;
+        Char = IntegerToWCharTable[Digit];
+        *Dest-- = Char;
+    } while (Value != 0);
+
+    //
+    // Pad the string with zeros if necessary.
+    //
+
+    NumberOfZerosToPad = NumberOfDigits - ActualNumberOfDigits;
+
+    if (NumberOfZerosToPad) {
+        do {
+            Count++;
+            Bytes += 2;
+            *Dest-- = L'0';
+        } while (--NumberOfZerosToPad);
+    }
+
+    //
+    // Update the string with the new length.
+    //
+
+    String->Length += (USHORT)Bytes;
+
+    //
+    // Add the trailer if applicable.
+    //
+
+    if (Trailer) {
+        String->Length += sizeof(WCHAR);
+        String->Buffer[(String->Length - 1) >> 1] = Trailer;
+    }
+
+    return TRUE;
+}
 
 _Use_decl_annotations_
 VOID
--- src/PerfectHash/RtlOutput.h
+++ src/PerfectHash/RtlOutput.h
@@ -55,289 +55,30 @@ CountNumberOfLongLongDigitsInline(_In_ ULONGLONG Value)
     return Count;
 }
 
-FORCEINLINE
+//
+// Helper string routines for buffer manipulation.
+//
+
+typedef
 BOOLEAN
-AppendIntegerToUnicodeString(
+(NTAPI APPEND_INTEGER_TO_UNICODE_STRING)(
     _In_ PUNICODE_STRING String,
     _In_ ULONG Integer,
     _In_ USHORT NumberOfDigits,
     _In_opt_ WCHAR Trailer
-    )
-/*++
-
-Routine Description:
-
-    This is a helper routine that allows construction of unicode strings out
-    of integer values.
-
-Arguments:
-
-    String - Supplies a pointer to a UNICODE_STRING that will be appended to.
-        Sufficient buffer space must exist for the entire string to be written.
-
-    Integer - The integer value to be appended to the string.
-
-    NumberOfDigits - The expected number of digits for the value.  If Integer
-        has less digits than this number, it will be left-padded with zeros.
-
-    Trailer - An optional trailing wide character to append.
-
-Return Value:
-
-    TRUE on success, FALSE on failure.
-
---*/
-{
-    USHORT ActualNumberOfDigits;
-    USHORT BytesRequired;
-    USHORT BytesRemaining;
-    USHORT NumberOfZerosToPad;
-    const ULONG Base = 10;
-    ULONG Digit;
-    ULONG Value;
-    ULONG Count;
-    ULONG Bytes;
-    WCHAR Char;
-    PWCHAR Dest;
-
-    //
-    // Verify the unicode string has sufficient space.
-    //
-
-    BytesRequired = NumberOfDigits * sizeof(WCHAR);
-
-    if (Trailer) {
-        BytesRequired += (1 * sizeof(Trailer));
-    }
-
-    BytesRemaining = (
-        String->MaximumLength -
-        String->Length
     );
+typedef APPEND_INTEGER_TO_UNICODE_STRING *PAPPEND_INTEGER_TO_UNICODE_STRING;
 
-    if (BytesRemaining < BytesRequired) {
-        return FALSE;
-    }
-
-    //
-    // Make sure the integer value doesn't have more digits than
-    // specified.
-    //
-
-    ActualNumberOfDigits = CountNumberOfDigitsInline(Integer);
-
-    if (ActualNumberOfDigits > NumberOfDigits) {
-        return FALSE;
-    }
-
-    //
-    // Initialize our destination pointer to the last digit.  (We write
-    // back-to-front.)
-    //
-
-    Dest = (PWCHAR)(
-        RtlOffsetToPointer(
-            String->Buffer,
-            String->Length + (
-                (NumberOfDigits - 1) *
-                sizeof(WCHAR)
-            )
-        )
-    );
-    Count = 0;
-    Bytes = 0;
-
-    //
-    // Convert each digit into the corresponding character and copy to the
-    // string buffer, retreating the pointer as we go.
-    //
-
-    Value = Integer;
-
-    do {
-        Count++;
-        Bytes += 2;
-        Digit = Value % Base;
-        Value = Value / Base;
-        Char = IntegerToWCharTable[Digit];
-        *Dest-- = Char;
-    } while (Value != 0);
-
-    //
-    // Pad the string with zeros if necessary.
-    //
-
-    NumberOfZerosToPad = NumberOfDigits - ActualNumberOfDigits;
-
-    if (NumberOfZerosToPad) {
-        do {
-            Count++;
-            Bytes += 2;
-            *Dest-- = L'0';
-        } while (--NumberOfZerosToPad);
-    }
-
-    //
-    // Update the string with the new length.
-    //
-
-    String->Length += (USHORT)Bytes;
-
-    //
-    // Add the trailer if applicable.
-    //
-
-    if (Trailer) {
-        String->Length += sizeof(WCHAR);
-        String->Buffer[(String->Length - 1) >> 1] = Trailer;
-    }
-
-    return TRUE;
-}
-
-FORCEINLINE
+typedef
 BOOLEAN
-AppendLongLongIntegerToUnicodeString(
+(NTAPI APPEND_LONGLONG_INTEGER_TO_UNICODE_STRING)(
     _In_ PUNICODE_STRING String,
     _In_ ULONGLONG Integer,
     _In_ USHORT NumberOfDigits,
     _In_opt_ WCHAR Trailer
-    )
-/*++
-
-Routine Description:
-
-    This is a helper routine that allows construction of unicode strings out
-    of integer values.
-
-Arguments:
-
-    String - Supplies a pointer to a UNICODE_STRING that will be appended to.
-        Sufficient buffer space must exist for the entire string to be written.
-
-    Integer - Supplies the long long integer value to be appended to the string.
-
-    NumberOfDigits - The expected number of digits for the value.  If Integer
-        has less digits than this number, it will be left-padded with zeros.
-
-    Trailer - An optional trailing wide character to append.
-
-Return Value:
-
-    TRUE on success, FALSE on failure.
-
---*/
-{
-    USHORT ActualNumberOfDigits;
-    USHORT BytesRequired;
-    USHORT BytesRemaining;
-    USHORT NumberOfZerosToPad;
-    const ULONGLONG Base = 10;
-    ULONGLONG Digit;
-    ULONGLONG Value;
-    ULONGLONG Count;
-    ULONGLONG Bytes;
-    WCHAR Char;
-    PWCHAR Dest;
-
-    //
-    // Verify the unicode string has sufficient space.
-    //
-
-    BytesRequired = NumberOfDigits * sizeof(WCHAR);
-
-    if (Trailer) {
-        BytesRequired += (1 * sizeof(Trailer));
-    }
-
-    BytesRemaining = (
-        String->MaximumLength -
-        String->Length
     );
-
-    if (BytesRemaining < BytesRequired) {
-        return FALSE;
-    }
-
-    //
-    // Make sure the integer value doesn't have more digits than
-    // specified.
-    //
-
-    ActualNumberOfDigits = CountNumberOfLongLongDigitsInline(Integer);
-
-    if (ActualNumberOfDigits > NumberOfDigits) {
-        return FALSE;
-    }
-
-    //
-    // Initialize our destination pointer to the last digit.  (We write
-    // back-to-front.)
-    //
-
-    Dest = (PWCHAR)(
-        RtlOffsetToPointer(
-            String->Buffer,
-            String->Length + (
-                (NumberOfDigits - 1) *
-                sizeof(WCHAR)
-            )
-        )
-    );
-    Count = 0;
-    Bytes = 0;
-
-    //
-    // Convert each digit into the corresponding character and copy to the
-    // string buffer, retreating the pointer as we go.
-    //
-
-    Value = Integer;
-
-    do {
-        Count++;
-        Bytes += 2;
-        Digit = Value % Base;
-        Value = Value / Base;
-        Char = IntegerToWCharTable[Digit];
-        *Dest-- = Char;
-    } while (Value != 0);
-
-    //
-    // Pad the string with zeros if necessary.
-    //
-
-    NumberOfZerosToPad = NumberOfDigits - ActualNumberOfDigits;
-
-    if (NumberOfZerosToPad) {
-        do {
-            Count++;
-            Bytes += 2;
-            *Dest-- = L'0';
-        } while (--NumberOfZerosToPad);
-    }
-
-    //
-    // Update the string with the new length.
-    //
-
-    String->Length += (USHORT)Bytes;
-
-    //
-    // Add the trailer if applicable.
-    //
-
-    if (Trailer) {
-        String->Length += sizeof(WCHAR);
-        String->Buffer[(String->Length - 1) >> 1] = Trailer;
-    }
-
-    return TRUE;
-}
-
-//
-// Helper string routines for buffer manipulation.
-//
+typedef APPEND_LONGLONG_INTEGER_TO_UNICODE_STRING
+      *PAPPEND_LONGLONG_INTEGER_TO_UNICODE_STRING;
 
 typedef
 VOID
@@ -461,6 +202,9 @@ VOID
 typedef APPEND_WIDE_CSTR_TO_WIDE_CHAR_BUFFER
       *PAPPEND_WIDE_CSTR_TO_WIDE_CHAR_BUFFER;
 
+extern APPEND_INTEGER_TO_UNICODE_STRING AppendIntegerToUnicodeString;
+extern APPEND_LONGLONG_INTEGER_TO_UNICODE_STRING
+    AppendLongLongIntegerToUnicodeString;
 extern APPEND_INTEGER_TO_CHAR_BUFFER AppendIntegerToCharBuffer;
 extern APPEND_INTEGER_TO_CHAR_BUFFER_AS_HEX AppendIntegerToCharBufferAsHex;
 extern APPEND_INTEGER_TO_CHAR_BUFFER_EX AppendIntegerToCharBufferEx;
