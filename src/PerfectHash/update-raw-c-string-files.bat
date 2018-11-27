b429e43cd27489d12dd97d36a860c98ae4797a18
Trent Nelson <trent@trent.me>
2018-11-27 09:27:58 +0800

    Remove Usage.txt from .bat file.

--- src/PerfectHash/update-raw-c-string-files.bat
+++ src/PerfectHash/update-raw-c-string-files.bat
@@ -1,9 +1,5 @@
 @echo off
 
-for /f "usebackq" %%i in (`dir /b *Usage.txt`) do (
-    cmd /c ph update-raw-c-string-file -i %%i
-)
-
 for /f "usebackq" %%i in (`dir /b ..\..\include\CompiledPerfectHash*.*`) do (
     cmd /c ph update-raw-c-string-file -i ..\..\include\%%i
 )
