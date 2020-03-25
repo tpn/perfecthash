519742407ed8828f932192e64eb9ed9958259c49
Trent Nelson <trent@trent.me>
2020-03-24 17:39:44 -0700

    Add run-pgi.bat.

---
+++ src/run-pgi.bat
@@ -0,0 +1,1 @@
+timemem src\x64\PGInstrument\PerfectHashBulkCreate.exe c:\src\perfecthash-keys\hard c:\temp\hard.0 Chm01 RotateMultiplyXorRotate2 And 3 --AttemptsBeforeTableResize=10000 --MaxNumberOfTableResizes=0 --FindBestGraph --BestCoverageAttempts=2 --BestCoverageType=HighestMaxGraphTraversalDepth --MainWorkThreadpoolPriority=Low
