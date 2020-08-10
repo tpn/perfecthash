8bce5eaccc079858e4fd09f48a2466391764b548
Trent Nelson <trent@trent.me>
2020-08-10 15:35:46 -0700

    Tweak build-ptx.bat.

--- src/PerfectHash/build-ptx.bat
+++ src/PerfectHash/build-ptx.bat
@@ -5,21 +5,40 @@ rem @echo off
 @for %%s in (%sources%) do (
     nvcc -ptx ..\PerfectHashCuda\%%s.cu                 ^
         -o ..\CompiledPerfectHashTable\%%sDebug.ptx     ^
+        -I../../include                                 ^
         -Wno-deprecated-gpu-targets                     ^
-        --cudart=none                                   ^
         --device-debug                                  ^
-        --restrict
+        --restrict                                      ^
+        -rdc=true                                       ^
+        --source-in-ptx                                 ^
+        --keep                                          ^
+        -maxrregcount=0                                 ^
+        --ptxas-options=-v                              ^
+        --machine 64                                    ^
+        -ptx -cudart shared                             ^
+        -gencode=arch=compute_35,code=\"sm_35,compute_35\"
 
-    cmd /c ph update-raw-c-string-file -i ..\CompiledPerfectHashTable\%%sDebug.ptx
+
+    rem Currently disabled due to .ptx lines being too long for msvc.
+    rem cmd /c ph update-raw-c-string-file -i ..\CompiledPerfectHashTable\%%sDebug.ptx
 
     nvcc -ptx ..\PerfectHashCuda\%%s.cu                 ^
         -o ..\CompiledPerfectHashTable\%%sRelease.ptx   ^
         -Wno-deprecated-gpu-targets                     ^
+        -I../../include                                 ^
         --optimize 3                                    ^
         --generate-line-info                            ^
-        --restrict
+        --restrict                                      ^
+        -rdc=true                                       ^
+        --source-in-ptx                                 ^
+        --keep                                          ^
+        -maxrregcount=0                                 ^
+        --ptxas-options=-v                              ^
+        --machine 64                                    ^
+        -ptx -cudart shared                             ^
+        -gencode=arch=compute_35,code=\"sm_35,compute_35\"
 
-    cmd /c ph update-raw-c-string-file -i ..\CompiledPerfectHashTable\%%sRelease.ptx
+    rem cmd /c ph update-raw-c-string-file -i ..\CompiledPerfectHashTable\%%sRelease.ptx
 )
 
 
