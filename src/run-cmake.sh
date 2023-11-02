e4f4b6a5723c3f531f1435ad5c7d3037ce34abed
Trent Nelson <trent@trent.me>
2023-11-01 22:06:06 -0700

    Tweak run-cmake.sh

--- src/run-cmake.sh
+++ src/run-cmake.sh
@@ -1,9 +1,17 @@
 #/bin/sh
 
-_ARCH=70
+_ARCH=${CUDA_ARCH:-70}
+
+if [ -d build.debug ]; then
+    rm -rf build.debug
+fi
 
 mkdir -p build.debug
 cmake -B build.debug -G "Ninja Multi-Config" -DCMAKE_CUDA_ARCHITECTURES=$_ARCH -Dstdgpu_DIR=$(realpath ../../stdgpu/debug/lib/cmake/stdgpu/)
+
+if [ -d build.release ]; then
+    rm -rf build.release
+fi
 
 mkdir -p build.release
 cmake -B build.release -G "Ninja Multi-Config" -DCMAKE_CUDA_ARCHITECTURES=$_ARCH -Dstdgpu_DIR=$(realpath ../../stdgpu/release/lib/cmake/stdgpu/)
