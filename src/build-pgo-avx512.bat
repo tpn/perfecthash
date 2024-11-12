1f616bc2a4adc82aa8cb771b0994836ea9e98c30
Trent Nelson <trent@trent.me>
2024-11-11 19:59:10 -0800

    Tweak the PGO build scripts.

--- src/build-pgo-avx512.bat
+++ src/build-pgo-avx512.bat
@@ -7,7 +7,7 @@ del /q .\x64\PGInstrument\*.pgc
 
 msbuild /nologo /m /t:Build /p:Configuration=PGInstrument;Platform=x64;InstructionSet=AdvancedVectorExtensions512 PerfectHash.sln
 ..\bin\timemem.exe .\x64\PGInstrument\PerfectHashCreate.exe c:\src\perfecthash-keys\sys32\HologramWorld-31016.keys c:\temp\ph.pgi Chm01 MultiplyShiftR And 12 --FindBestGraph --BestCoverageType=HighestScore --BestCoverageAttempts=4 --HashAllKeysFirst --TryUseAvx2HashFunction --InitialNumberOfTableResizes=1
-..\bin\timemem.exe .\x64\PGInstrument\PerfectHashBulkCreate.exe c:\src\perfecthash-keys\hard c:\temp\ph.pgi Chm01 RotateMultiplyXorRotate2 And 3 --AttemptsBeforeTableResize=10000 --MaxNumberOfTableResizes=0 --FindBestGraph --BestCoverageAttempts=2 --BestCoverageType=HighestNumberOfEmptyCacheLines --MainWorkThreadpoolPriority=Low
+..\bin\timemem.exe .\x64\PGInstrument\PerfectHashBulkCreate.exe c:\src\perfecthash-keys\hard c:\temp\ph.pgi Chm01 RotateMultiplyXorRotate2 And 3 --AttemptsBeforeTableResize=10000 --MaxNumberOfTableResizes=0 --FindBestGraph --BestCoverageAttempts=2 --BestCoverageType=HighestScore --MainWorkThreadpoolPriority=Low
 
 @echo off
 
