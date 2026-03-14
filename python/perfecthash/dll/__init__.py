2cbba14d06aa929417d90fcfb651c32248e6cbe3
Trent Nelson <trent@trent.me>
2026-03-14 07:55:07 -0700

    Removing old Python code.

--- python/perfecthash/dll/__init__.py
+++
@@ -1,3 +0,0 @@
-
-
-# vim:set ts=8 sw=4 sts=4 tw=80 et                                             :
--- python/setup.py
+++
@@ -1,9 +0,0 @@
-# `python setup.py build_ext --inplace`
-import numpy as np
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    ext_modules=cythonize(['perfecthash/_graph.pyx'], annotate=True),
-    include_dirs=[np.get_include()],
-)
